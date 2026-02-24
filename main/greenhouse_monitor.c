33bfe1174906a62ae32705a9fa4cdc25dee67607
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2026-02-24 21:31:02 +0200

    feat: add production mode with deep sleep and retry logic
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- main/greenhouse_monitor.c
+++ main/greenhouse_monitor.c
@@ -1,6 +1,9 @@
 #include "driver/gpio.h"
+#include "esp_interface.h"
 #include "esp_log.h"
+#include "esp_sleep.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
 
@@ -10,22 +13,17 @@
 #include "sensors.h"
 #include "util.h"
 
+#define DEEP_SLEEP_DURATION_SEC 1800
+#define WARM_UP_DURATION_SEC 180
 #define NR_RETIRES 5
 
 static const char *TAG = "MAIN";
 
-void app_main()
+// #define TEST_MODE // comment to deactivate test mode
+
+#ifdef TEST_MODE
+void test()
 {
-	// power on sensors
-	gpio_reset_pin(NPN_PIN);
-	gpio_set_direction(NPN_PIN, GPIO_MODE_OUTPUT);
-	gpio_set_level(NPN_PIN, 1);
-
-	ESP_ERROR_CHECK(nvs_flash_init());
-	init_wifi();
-	init_sntp();
-	init_adc();
-
 	sensor_data_t data;
 	int sock;
 
@@ -80,4 +78,89 @@ void app_main()
 
 	ESP_LOGI(TAG, "Disconnecting from server...");
 	disconnect(sock);
+}
+#endif
+
+void app_main()
+{
+	// power on sensors
+	gpio_reset_pin(NPN_PIN);
+	gpio_set_direction(NPN_PIN, GPIO_MODE_OUTPUT);
+	gpio_set_level(NPN_PIN, 1);
+
+	// wait for warmup
+	// vTaskDelay(pdMS_TO_TICKS(1000 * WARM_UP_DURATION_SEC));
+
+	ESP_ERROR_CHECK(nvs_flash_init()); // needed for wifi
+	init_wifi();
+	init_sntp(); // needed for time sync
+	init_adc();
+
+#ifdef TEST_MODE
+	test();
+#else
+	esp_log_level_set("*", ESP_LOG_NONE); // disable all logs
+
+	sensor_data_t data;
+	esp_err_t status = ESP_FAIL;
+
+	// try reading from dht
+	for (int attempt = 0; attempt < NR_RETIRES; ++attempt) {
+		status =
+			dht11_get_temp_and_humidity(DHT_PIN, &data.temp, &data.humidity);
+		if (status == ESP_OK) {
+			break;
+		}
+		ESP_LOGW(TAG, "Attempt %d failed, retrying...", attempt + 1);
+		vTaskDelay(pdMS_TO_TICKS(2000)); // wait before retry
+	}
+
+	// if reading fails we don't send the data and go imidiatelly to sleep
+	if (status != ESP_OK) {
+		ESP_LOGE(TAG,
+				 "All attempts to read DHT11 failed. Going to deep sleep.");
+		goto sleep;
+	}
+
+	// read ch4
+	data.ch4 = mq4_get_ppm(adc_handle, MQ4_PIN);
+	data.co2 = mq135_get_ppm(adc_handle, MQ135_PIN);
+	data.timestamp = time(NULL);
+
+	ESP_LOGI(TAG,
+			 "Sensor Read: Temp=%d Humidity=%d CH4=%.2f CO2=%0.2f - %lld",
+			 data.temp,
+			 data.humidity,
+			 data.ch4,
+			 data.co2,
+			 data.timestamp);
+
+	// try sending the data to the server
+	int sock;
+	bool sent = false;
+
+	for (int attempt = 0; attempt < NR_RETIRES; ++attempt) {
+		if (my_connect(&sock) == ESP_OK) {
+			if (send_all(sock, &data, sizeof(data)) == ESP_OK) {
+				ESP_LOGI(TAG, "Data sent successfully.");
+				sent = true;
+			}
+			disconnect(sock);
+			break;
+		}
+		ESP_LOGW(TAG, "Attempt %d failed, retrying...", attempt + 1);
+		vTaskDelay(pdMS_TO_TICKS(2000));
+	}
+
+	if (!sent) {
+		ESP_LOGW(TAG, "All atempts to send data failed.");
+	}
+
+sleep:
+	gpio_set_level(NPN_PIN, 0);
+	ESP_LOGI(TAG,
+			 "Entering deep sleep for %d seconds.",
+			 DEEP_SLEEP_DURATION_SEC);
+	esp_deep_sleep(1000000 * DEEP_SLEEP_DURATION_SEC);
+#endif
 }
